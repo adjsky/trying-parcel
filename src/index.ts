8b73d76b17348991da7f2e740e35fdb1c64d56d0
adjsky <igorlfmartins@mail.ru>
2023-02-04 10:41:49 +0300

    add new es stuff, check building for different targets

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export function helloWorld() {
+export const helloWorld = () => {
   console.log("Hello, World!");
+};
+
+export const asyncHelloWorld = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  console.log("Hello, World!");
+};
+
+export class TestClass {
+  private field = "wqe";
 }
 
 export default helloWorld;
